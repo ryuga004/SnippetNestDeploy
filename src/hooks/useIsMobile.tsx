054aba2a7c82acd8197c151c7714ff117140b226
ryuga <rahul0singh001@gmail.com>
2025-03-01 19:13:30 +0530

    added comment
    Co-authored-by: Rahul Singh <ryuga004@gmail.com>

--- src/hooks/useIsMobile.tsx
+++ src/hooks/useIsMobile.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const useIsMobile = (breakpoint: number = 1100) => {
     const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
-
+    // custom hook to added for media query usage 
     useEffect(() => {
         const handleResize = () => {
             setIsMobile(window.innerWidth < breakpoint);
