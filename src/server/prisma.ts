ef767e10c0ecb6b1134f7c30f7f3bee0190fa59c
ryuga <rahul0singh001@gmail.com>
2025-03-13 23:19:55 +0530

    check 13

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
 
 export default prisma;
