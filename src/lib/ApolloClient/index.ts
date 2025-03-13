240ac7ae00cafee92ee22e45ff8b425e7f73a50c
ryuga <rahul0singh001@gmail.com>
2025-03-13 20:39:29 +0530

    check 6

--- src/lib/ApolloClient/index.ts
+++ src/lib/ApolloClient/index.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
     link: new HttpLink({
-        uri: "/api/graphql",
+        uri: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/graphql",
         credentials: "include",
     }),
     cache: new InMemoryCache(),
