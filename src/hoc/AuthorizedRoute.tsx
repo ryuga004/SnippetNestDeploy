549fb9fda1f50b0c3d715b375ab73ae4668cfde7
Rahul Singh <rahul0singh001@gmail.com>
2025-04-01 21:41:18 +0530

    added comment

--- src/hoc/AuthorizedRoute.tsx
+++ src/hoc/AuthorizedRoute.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AuthorizedRoute = () => {
-  return <div>AuthorizedRouteOnly</div>;
+  return <div>AuthorizedRoutesOnly</div>;
 };
 
 export default AuthorizedRoute;
