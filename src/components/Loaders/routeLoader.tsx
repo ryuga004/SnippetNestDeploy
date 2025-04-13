a4bcd647e8187c00a099411c6026222fd44eb2d3
Rahul Singh <rahul0singh001@gmail.com>
2025-04-13 21:07:58 +0530

    added a new comment

--- src/components/Loaders/routeLoader.tsx
+++ src/components/Loaders/routeLoader.tsx
@@ -3,30 +3,33 @@
 import { useEffect, useState, useTransition } from "react";
 import { usePathname } from "next/navigation";
 import { Progress } from "@/components/ui/progress";
-
+// yet to implement with frontend ....
 const RouteLoader = () => {
-    const pathname = usePathname();
-    const [progress, setProgress] = useState(0);
-    const [loading, startTransition] = useTransition();
-    console.log(loading);
-    useEffect(() => {
-        startTransition(() => {
-            setProgress(20);
-        });
+  const pathname = usePathname();
+  const [progress, setProgress] = useState(0);
+  const [loading, startTransition] = useTransition();
+  console.log(loading);
+  useEffect(() => {
+    startTransition(() => {
+      setProgress(20);
+    });
 
-        return () => {
-            setProgress(100);
-            setTimeout(() => setProgress(0), 300); // Reset
-        };
-    }, [pathname]);
+    return () => {
+      setProgress(100);
+      setTimeout(() => setProgress(0), 300); // Reset
+    };
+  }, [pathname]);
 
-    if (progress === 0) return null;
+  if (progress === 0) return null;
 
-    return (
-        <div className="fixed top-0 left-0 w-full h-1 z-50">
-            <Progress value={progress} className="h-1 bg-blue-500 transition-all duration-300" />
-        </div>
-    );
+  return (
+    <div className="fixed top-0 left-0 w-full h-1 z-50">
+      <Progress
+        value={progress}
+        className="h-1 bg-blue-500 transition-all duration-300"
+      />
+    </div>
+  );
 };
 
 export default RouteLoader;
